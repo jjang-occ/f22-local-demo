8b7244b34afa6e6d0ece6f49a18418ef61fc9a94
Nguyen <nnguyen1453@student.cccd.edu>
2022-12-06 16:20:16 -0800

    add another thing

--- Main.cpp
+++ Main.cpp
@@ -3,8 +3,9 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello! It's me.";
+	cout << "Hello! It's me." << endl;
 	cout << "A cool feature." << endl;
 	cout << "More cool stuff" << endl;
+	cout << "Add one more" << endl;
 	return 0;
 }
